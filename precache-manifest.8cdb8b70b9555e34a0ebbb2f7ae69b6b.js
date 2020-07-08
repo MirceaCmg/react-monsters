48fc88e189500ba675046baac9389492d468096a
Mircea Cmg <mirceacmg@192-168-0-101.rdsnet.ro>
2020-07-08 21:02:36 +0300

    Updates

---
+++ precache-manifest.8cdb8b70b9555e34a0ebbb2f7ae69b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d7480b28d2724e8c15868a1f7e26d3",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "cfc107f61ed3e1feaeab",
+    "url": "/react-monsters/static/css/main.420df3c6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc107f61ed3e1feaeab",
+    "url": "/react-monsters/static/js/main.3733c80b.chunk.js"
+  },
+  {
+    "revision": "9e48f53adff59196fe9e",
+    "url": "/react-monsters/static/js/runtime-main.265b368b.js"
+  }
+]);
